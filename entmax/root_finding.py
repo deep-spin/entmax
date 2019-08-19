53c07f3c57360f0db53a74617f2fbb10becc4c41
Vlad Niculae <vlad@vene.ro>
2019-08-19 19:00:05 +0100

    add root finding with learned alpha and unit tests

--- entmax/root_finding.py
+++ entmax/root_finding.py
@@ -3,48 +3,37 @@ import torch
 from torch.autograd import Function
 
 
-def assert_equal(*args):
-    """
-    Assert all arguments have the same value
-    """
-    arguments = (arg for arg in args)
-    first = next(arguments)
-    assert all(
-        arg == first for arg in arguments
-    ), "Not all arguments have the same value: " + str(args)
+class EntmaxBisectFunction(Function):
 
+    @classmethod
+    def _gp(cls, x, alpha):
+        return x ** (alpha - 1)
 
-def _p(x, tau):
-    return torch.clamp(x - tau, min=0)
+    @classmethod
+    def _gp_inv(cls, y, alpha): return y ** (1 / (alpha - 1))
 
+    @classmethod
+    def _p(cls, X, alpha):
+        return cls._gp_inv(torch.clamp(X, min=0), alpha)
 
-def _entmax_gp(x, alpha):
-    return x ** (alpha - 1)
+    @classmethod
+    def forward(cls, ctx, X, alpha=1.5, n_iter=50, ensure_sum_one=True):
 
-
-def _entmax_gp_inv(y, alpha):
-    return y ** (1 / (alpha - 1))
-
-
-def _entmax_p(X, alpha):
-    return _entmax_gp_inv(torch.clamp(X, min=0), alpha)
-
-
-# TODO: support other dims other than 1. The code likely same, but needs tested
-class SparsemaxBisectFunction(Function):
-    @staticmethod
-    def forward(ctx, X, n_iter=50, ensure_sum_one=True):
-
+        if not isinstance(alpha, torch.Tensor):
+            alpha = torch.tensor(alpha, dtype=X.dtype)
+        alpha = alpha.unsqueeze(-1)  # different alpha per row
+        ctx.alpha = alpha
         ctx.dim = dim = 1
         d = X.shape[dim]
 
+        X = X * (alpha - 1)
+
         max_val, _ = X.max(dim=dim, keepdim=True)
 
-        tau_lo = max_val - 1
-        tau_hi = max_val - (1 / d)
+        tau_lo = max_val - cls._gp(1, alpha)
+        tau_hi = max_val - cls._gp(1 / d, alpha)
 
-        f_lo = _p(X, tau_lo).sum(dim) - 1
-        f_hi = _p(X, tau_hi).sum(dim) - 1
+        f_lo = cls._p(X - tau_lo, alpha).sum(dim) - 1
 
         dm = tau_hi - tau_lo
 
@@ -52,7 +41,7 @@ class SparsemaxBisectFunction(Function):
 
             dm /= 2
             tau_m = tau_lo + dm
-            p_m = _p(X, tau_m)
+            p_m = cls._p(X - tau_m, alpha)
             f_m = p_m.sum(dim) - 1
 
             mask = (f_m * f_lo >= 0).unsqueeze(dim)
@@ -64,6 +53,58 @@ class SparsemaxBisectFunction(Function):
         ctx.save_for_backward(p_m)
 
         return p_m
+
+    @classmethod
+    def backward(cls, ctx, dY):
+        Y, = ctx.saved_tensors
+
+        gppr = torch.where(Y > 0, Y ** (2 - ctx.alpha), Y.new_zeros(1))
+
+        dX = dY * gppr
+        q = dX.sum(ctx.dim) / gppr.sum(ctx.dim)
+        q = q.unsqueeze(ctx.dim)
+        dX -= q * gppr
+
+        d_alpha = None
+        if ctx.needs_input_grad[1]:
+
+            # alpha gradient computation
+            # d_alpha = (partial_y / partial_alpha) * dY
+            # NOTE: ensure alpha is not close to 1
+            # since there is an indetermination
+            batch_size, _ = dY.shape
+
+            # shannon terms
+            S = torch.where(Y > 0, Y * torch.log(Y), Y.new_zeros(1))
+            # shannon entropy
+            ent = S.sum(ctx.dim).unsqueeze(-1)
+            Y_skewed = gppr / gppr.sum(ctx.dim).unsqueeze(-1)
+            d_alpha = dY * (Y - Y_skewed) / ((ctx.alpha - 1) ** 2)
+            d_alpha -= dY * (S - Y_skewed * ent) / (ctx.alpha - 1)
+            d_alpha = d_alpha.sum(dim=-1)
+
+        return dX, d_alpha, None, None
+
+
+# slightly more efficient special case for sparsemax
+class SparsemaxBisectFunction(EntmaxBisectFunction):
+
+    @classmethod
+    def _gp(cls, x, alpha):
+        return x
+
+    @classmethod
+    def _gp_inv(cls, y, alpha):
+        return y
+
+
+    @classmethod
+    def _p(cls, x, alpha):
+        return torch.clamp(x, min=0)
+
+    @classmethod
+    def forward(cls, ctx, X, n_iter=50, ensure_sum_one=True):
+        return super().forward(ctx, X, alpha=2, n_iter=50, ensure_sum_one=True)
 
     @staticmethod
     def backward(ctx, dY):
@@ -73,60 +114,80 @@ class SparsemaxBisectFunction(Function):
         q = dX.sum(ctx.dim) / gppr.sum(ctx.dim)
         q = q.unsqueeze(ctx.dim)
         dX -= q * gppr
-        return dX, None
-
-
-class EntmaxBisectFunction(Function):
-    @staticmethod
-    def forward(ctx, X, alpha=1.5, n_iter=50, ensure_sum_one=True):
-
-        ctx.alpha = alpha
-        ctx.dim = dim = 1
-        d = X.shape[dim]
-
-        X = X * (alpha - 1)
-
-        max_val, _ = X.max(dim=dim, keepdim=True)
-
-        minv = _entmax_gp(0, alpha)
-
-        tau_lo = max_val - _entmax_gp(1, alpha)
-        tau_hi = max_val - _entmax_gp(1 / d, alpha)
-
-        f_lo = _entmax_p(X - tau_lo, alpha).sum(dim) - 1
-        f_hi = _entmax_p(X - tau_hi, alpha).sum(dim) - 1
-
-        dm = tau_hi - tau_lo
-
-        for it in range(n_iter):
-
-            dm /= 2
-            tau_m = tau_lo + dm
-            p_m = _entmax_p(X - tau_m, alpha)
-            f_m = p_m.sum(dim) - 1
-
-            mask = (f_m * f_lo >= 0).unsqueeze(dim)
-            tau_lo = torch.where(mask, tau_m, tau_lo)
-
-        if ensure_sum_one:
-            p_m /= p_m.sum(dim=1).unsqueeze(dim=1)
-
-        ctx.save_for_backward(p_m)
-
-        return p_m
-
-    @staticmethod
-    def backward(ctx, dY):
-        Y, = ctx.saved_tensors
-
-        gppr = torch.where(Y > 0, Y ** (2 - ctx.alpha), Y.new_zeros(1))
-
-        dX = dY * gppr
-        q = dX.sum(ctx.dim) / gppr.sum(ctx.dim)
-        q = q.unsqueeze(ctx.dim)
-        dX -= q * gppr
         return dX, None, None
 
 
-sparsemax_bisect = SparsemaxBisectFunction.apply
-entmax_bisect = EntmaxBisectFunction.apply
+def entmax_bisect(X, alpha=1.5, n_iter=50, ensure_sum_one=True):
+    """alpha-entmax: normalizing sparse transform (a la softmax).
+
+    Solves the optimization problem:
+
+        max_p <x, p> - H_a(p)    s.t.    p >= 0, sum(p) == 1.
+
+    where H_a(p) is the Tsallis alpha-entropy with custom alpha >= 1,
+    using a bisection (root finding, binary search) algorithm.
+
+    This function is differentiable with respect to both X and alpha.
+
+    Note: This function does not yet support normalizing along anything except
+    the last dimension. Please use transposing and views to achieve more general
+    behavior.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input 2D tensor, to be normalized along the last dimension.
+
+    alpha : float or torch.Tensor
+        Tensor of alpha parameters (> 1) to use for each row of X. If scalar
+        or python float, the same value is used for all rows. A value of
+        alpha=2 corresponds to sparsemax, and alpha=1 corresponds to softmax
+        (but computing it this way is likely unstable).
+
+    n_iter : int
+        Number of bisection iterations. For float32, 24 iterations should
+        suffice for machine precision.
+
+    ensure_sum_one : bool,
+        Whether to divide the result by its sum. If false, the result might
+        sum to close but not exactly 1, which might cause downstream problems.
+
+    Returns
+    -------
+    P : torch tensor, same shape as X
+        The projection result, such that P.sum(dim=-1) == 1 elementwise.
+    """
+    return EntmaxBisectFunction.apply(X, alpha, n_iter, ensure_sum_one)
+
+
+def sparsemax_bisect(X, n_iter=50, ensure_sum_one=True):
+    """sparsemax: normalizing sparse transform (a la softmax), via bisection.
+
+    Solves the projection:
+
+        min_p ||x - p||_2   s.t.    p >= 0, sum(p) == 1.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input tensor.
+
+    n_iter : int
+        Number of bisection iterations. For float32, 24 iterations should
+        suffice for machine precision.
+
+    ensure_sum_one : bool,
+        Whether to divide the result by its sum. If false, the result might
+        sum to close but not exactly 1, which might cause downstream problems.
+
+    Note: This function does not yet support normalizing along anything except
+    the last dimension. Please use transposing and views to achieve more general
+    behavior.
+
+    Returns
+    -------
+    P : torch tensor, same shape as X
+        The projection result, such that P.sum(dim=-1) == 1 elementwise.
+    """
+    return SparsemaxBisectFunction.apply(X, n_iter, ensure_sum_one)
+
--- entmax/test_root_finding.py
+++ entmax/test_root_finding.py
@@ -20,7 +20,7 @@ def test_entmax15():
     for _ in range(10):
         x = 0.5 * torch.randn(10, 30000, dtype=torch.float32)
         p1 = entmax15(x, 1)
-        p2 = entmax_bisect(x, 1.5)
+        p2 = entmax_bisect(x, alpha=1.5)
         assert torch.sum((p1 - p2) ** 2) < 1e-7
 
 
@@ -35,9 +35,30 @@ def test_sparsemax_grad():
 @pytest.mark.parametrize("alpha", (1.2, 1.5, 1.75, 2.25))
 def test_entmax_grad(alpha):
 
+    alpha = torch.tensor(alpha, dtype=torch.float64, requires_grad=True)
+
     for _ in range(10):
-
         x = torch.randn(4, 6, dtype=torch.float64, requires_grad=True)
-        _, y = torch.max(torch.randn_like(x), dim=1)
-
         gradcheck(entmax_bisect, (x, alpha), eps=1e-5)
+
+
+def test_entmax_correct_multiple_alphas():
+    n = 4
+    x = torch.randn(n, 6, dtype=torch.float64, requires_grad=True)
+    alpha = 1.05 + torch.rand(n, dtype=torch.float64, requires_grad=True)
+
+    p1 = entmax_bisect(x, alpha)
+    p2_ = [entmax_bisect(x[i].unsqueeze(0), alpha[i].item()).squeeze()
+            for i in range(n)]
+    p2 = torch.stack(p2_)
+
+    assert torch.allclose(p1, p2)
+
+
+def test_entmax_grad_multiple_alphas():
+
+    for _ in range(10):
+        x = torch.randn(4, 6, dtype=torch.float64, requires_grad=True)
+        alpha = 1.05 + torch.rand(4, dtype=torch.float64, requires_grad=True)
+        gradcheck(entmax_bisect, (x, alpha), eps=1e-5)
+
