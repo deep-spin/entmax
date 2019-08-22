e5ef675a78fab1f791395bba689f93bd429aa7b5
Vlad Niculae <vlad@vene.ro>
2019-08-22 16:17:20 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='entmax',
+      version='1.0dev0',
+      author="Ben Peters, Goncalo M Correia, Vlad Niculae",
+      description=("The entmax mapping and its loss, a family of sparse "
+                   "softmax alternatives."),
+      license="MIT",
+      python_requires=">=3.5")
