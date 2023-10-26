07e08de2631195e43381da2391caa7880ceae3c2
Ben Peters <bpeters@coli.uni-saarland.de>
2023-10-26 15:43:01 +0100

    test on other kinds of tensors

--- entmax/test_amp.py
+++ entmax/test_amp.py
@@ -17,26 +17,34 @@ if torch.cuda.is_available():
     mappings = [entmax15, sparsemax, partial(entmax_bisect, alpha=1.5), partial(entmax_bisect, alpha=2)]
 
     # make data
-    Xs = [
-        torch.randn(1000, dtype=torch.float32, device="cuda")
+    long_vecs = [
+        torch.randn(32000, dtype=torch.float32, device="cuda")
         for _ in range(5)
     ]
 
+    negatives = []
+    for i in range(2, 6):
+        negative = torch.randn(128, dtype=torch.float32, device="cuda") - 10 ** i
+        negative[0] += 5
+        negatives.append(negative)
+
+    @pytest.mark.parametrize("Xs", (long_vecs, negatives))
     @pytest.mark.parametrize("func", mappings)
     @pytest.mark.parametrize("dtype", (torch.bfloat16, torch.float16))
-    def test_sum_one(func, dtype):
-        _Xs = [_X.to(dtype) for _X in Xs]
+    def test_sum_one(Xs, func, dtype):
+        _Xs = [X.to(dtype) for X in Xs]
         with torch.autocast(device_type="cuda", dtype=dtype):
             for _X in _Xs:
                 scores = func(_X, dim=-1)
                 prob_mass = scores.sum(-1)
                 assert torch.allclose(prob_mass, torch.tensor([1.0], device="cuda"))
 
+    @pytest.mark.parametrize("Xs", (long_vecs, negatives))
     @pytest.mark.parametrize("func", mappings)
     @pytest.mark.parametrize("dtype", (torch.bfloat16, torch.float16))
-    def test_probs_close(func, dtype):
-        full_precision_probs = [func(_X.to(dtype).to(torch.float32), dim=-1) for _X in Xs]
-        _Xs = [_X.to(dtype) for _X in Xs]
+    def test_probs_close(Xs, func, dtype):
+        full_precision_probs = [func(X.to(dtype).to(torch.float32), dim=-1) for X in Xs]
+        _Xs = [X.to(dtype) for X in Xs]
         with torch.autocast(device_type="cuda", dtype=dtype):
             for _X, fpp in zip(_Xs, full_precision_probs):
                 probs = func(_X, dim=-1)
